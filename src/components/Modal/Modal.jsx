8da291f6de46d002f62ac33792411ca873d59de2
filimon4uck <134231431+filimon4uck@users.noreply.github.com>
2024-02-21 10:16:15 +0100

    fix overlay

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,17 +2,17 @@ import style from './Modal.module.css';
 import { Component } from 'react';
 
 class Modal extends Component {
-  render() {
-    return (
-      <div className={`${style.overlay} js_overlay`}>
-        <div className={style.modal}>
-          <img width={700} height={500} src={this.props.largeImageURL} alt="" />
-        </div>
-      </div>
-    );
+  componentDidMount() {
+    document.documentElement.style.overflowY = 'hidden';
+    window.addEventListener('keydown', this.handlePressKey);
   }
+  componentWillUnmount() {
+    document.documentElement.style.overflowY = '';
+    window.removeEventListener('keydown', this.handlePressKey);
+  }
+
   handleClickOverlay = evn => {
-    if (!evn.target.classList.contains('js_overlay')) {
+    if (evn.target !== evn.currentTarget) {
       return;
     }
     this.props.onClose();
@@ -25,15 +25,14 @@ class Modal extends Component {
     this.props.onClose();
   };
 
-  componentDidMount() {
-    document.documentElement.style.overflowY = 'hidden';
-    window.addEventListener('click', this.handleClickOverlay);
-    window.addEventListener('keydown', this.handlePressKey);
-  }
-  componentWillUnmount() {
-    document.documentElement.style.overflowY = '';
-    window.removeEventListener('click', this.handleClickOverlay);
-    window.removeEventListener('keydown', this.handlePressKey);
+  render() {
+    return (
+      <div className={style.overlay} onClick={this.handleClickOverlay}>
+        <div className={style.modal}>
+          <img width={700} height={500} src={this.props.largeImageURL} alt="" />
+        </div>
+      </div>
+    );
   }
 }
 export default Modal;
